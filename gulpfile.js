1e05a77fa273d950f6d79431dd050855f0f9f44a
Julien Dubois <julien.dubois@gmail.com>
2015-10-03 13:19:49 +0200

    automatic project update

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-// Generated on 2015-10-01 using generator-jhipster 2.21.1
+// Generated on 2015-10-03 using generator-jhipster 2.21.1
 /* jshint camelcase: false */
 'use strict';
 
--- src/main/webapp/scripts/app/account/logout/logout.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('samplegulpApp')
-    .controller('LogoutController', function (Auth) {
-        Auth.logout();
-    });
